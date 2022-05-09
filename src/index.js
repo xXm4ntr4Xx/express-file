ab1cda53431f4d817b30315edbe02ba5cf9e1a76
Pietro Annobil <pietroannobil@Pietros-MacBook-Air.local>
2022-05-09 23:14:06 +0100

    g

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import cookieParser  from 'cookie-parser';
 import cors  from 'cors';
 import logger  from 'morgan';
-import pool from './db/connection.js'
+import pool from '../db/connection.js'
+import serverless from 'serverless-http'
 
 
 const app = express()
 const port = process.env.PORT || 3000
+const router = express.Router();
 
 app.use(logger('dev'));
 app.use(cors());
@@ -15,22 +17,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 
+app.use('/.netlify/functions/index',router)
+
 /* GET users listing. */
-app.get("/", async (req, res, next)=> {
+router.get("/", async (req, res, next)=> {
   res.json({ 
     message:'connected to database',
     link:'http://localhost:3000/drinks'
  });
 });
 
-app.get("/drinks", async (req, res, next)=> {
+router.get("/drinks", async (req, res, next)=> {
   const cocktailList = await pool.query('SELECT * FROM cocktails')
   res.json({ 
     payload:cocktailList.rows
  });
 });
 
-app.get("/drinks/:id", async (req, res, next)=> {
+router.get("/drinks/:id", async (req, res, next)=> {
   const id = req.params.id;
   const selectedCocktail = await pool.query('SELECT * FROM cocktails WHERE id = $1',[id])
   res.json({ 
@@ -39,7 +43,7 @@ app.get("/drinks/:id", async (req, res, next)=> {
  });
 });
 
-app.post('/drinks',async(req,res)=>{
+router.post('/drinks',async(req,res)=>{
   const{name,image,ingridients,instructions}=req.body;
   const saveCockatil = await pool.query('INSERT INTO cocktails(name,image,ingridients,instructions) VALUES($1,$2,$3,$4)',[name,image,ingridients,instructions])
   res.json({
@@ -48,7 +52,7 @@ app.post('/drinks',async(req,res)=>{
   })
 })
 
-app.delete("/drinks/:id",async(req,res)=>{
+router.delete("/drinks/:id",async(req,res)=>{
   const id = req.params.id;
   const deleteCocktail = await pool.query('DELETE FROM cocktails WHERE id = $1',[id])
   res.json({
@@ -60,3 +64,6 @@ app.delete("/drinks/:id",async(req,res)=>{
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+export default serverless(app);
