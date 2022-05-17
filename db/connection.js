f0d8687810135931b1becc63f56e3eef0880473c
Pietro Annobil <pietroannobil@Pietros-MacBook-Air.local>
2022-05-17 11:47:09 +0100

    a

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,21 @@
 import pg from "pg";
 
+// const pool = new pg.Pool({
+//   database:'postgres',
+//   user:'postgres',
+//   password:"300488",
+//   host:'localhost',
+//   port:5432,
+// });
+
 const pool = new pg.Pool({
-  database:'postgres',
-  user:'postgres',
-  password:"300488",
-  host:'localhost',
-  port:5432,
+  host:process.env.DB_HOST,
+  database:process.env.DB_DATABASE,
+  user:process.env.DB_USER,
+  port:process.env.DB_PORT,
+  password:process.env.DB_PASSWORD,
+  ssl:{rejectUnauthorized:false}
 });
 
+console.log(pool)
 export default pool;
--- db/scripts/users/createTable.js
+++ db/scripts/users/createTable.js
@@ -1,9 +1,14 @@
 import db from "../../connection.js";
 
-const response = db.query(
-  `CREATE TABLE IF NOT EXISTS cocktails (id SERIAL PRIMARY KEY, name TEXT, image TEXT, ingridients TEXT , instructions TEXT);`
-);
 
-console.log(response);
 
-db.end();
+const createTable = async ()=>{
+  const response = await db.query(
+    `CREATE TABLE IF NOT EXISTS cocktails (id SERIAL PRIMARY KEY, name TEXT, image TEXT, ingridients TEXT , instructions TEXT);`
+  );
+ console.log(response)
+}
+
+// db.end();
+
+createTable();
