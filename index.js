394913f18fee55b384c1c5f24c9ac98cd5c76727
Pietro Annobil <pietroannobil@Pietros-MacBook-Air.local>
2022-05-18 23:26:38 +0100

    add new project

--- index.js
+++ index.js
@@ -18,11 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next()
-});
 
 app.use('/',router)
 
