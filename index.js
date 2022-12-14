43a1d001c61b4d5e25be8610775ede04cf6ba609
Your Name <pietroannobil@Pietros-MacBook-Air.local>
2022-12-14 15:31:13 +0000

    up

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ router.get("/", async (req, res, next)=> {
 
 router.get(`/drinks`, async (req, res, next)=> {
   const cocktailList = await pool.query('SELECT * FROM cocktails')
+  alert("hello")
   res.json({ 
     payload:cocktailList.rows
  });
